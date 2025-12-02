0c6e97f64f0a9ed1aff9b7c1939d63ee3e145dca
mightymax <mark@lindeman.nu>
2025-12-02 17:58:45 +0100

    Implement Day 1 and Day 2 solutions with rotation logic and testing

--- src/2025/index.ts
+++ src/2025/index.ts
@@ -12,6 +12,7 @@ type Rotation = {
 
 const startPosition = 50;
 const positions = 100
+let countPosition0 = 0
 let passedPosition0Count = 0;
 
 const file = process.env.ENV === "test" ? "static/2025/day1.test.txt" : "static/2025/day1.input.txt";
@@ -45,20 +46,28 @@ const answer = await fs.readFile(file, "utf-8")
       }
       currentPosition = (currentPosition + (rotation.direction === "L" ? -rotation.steps : rotation.steps) + positions) % positions;
       console.log(`  - The dial is rotated ${chalk.bold(rotation.direction + rotation.steps)} to point at ${currentPosition === 0 ? chalk.yellow.bold(currentPosition) : currentPosition}${hasPast0Count > 0 ? `; during this rotation, it points at 0 ${hasPast0Count > 1 ? "times" : "once"}` : ""}.`)
-      if (currentPosition === 0 && hasPast0Count === 0) 
-        passedPosition0Count++
+      if (currentPosition === 0 && hasPast0Count === 0)  passedPosition0Count++
+      if (currentPosition === 0)  countPosition0++
     }
     console.log(chalk.cyan.bold(`  - Final position: ${currentPosition}`));
+    console.log(chalk.cyan.bold(`  - Times at position 0: ${countPosition0}`));
     console.log(chalk.cyan.bold(`  - Number of times passed position 0: ${passedPosition0Count}`));
-    return passedPosition0Count
+    return {day2: passedPosition0Count, day1: countPosition0}
   });
 
-console.log(chalk.yellow.bold(`Answer: ${answer}`));
+console.log(chalk.yellow.bold(`Answer: \n  - day1: ${answer.day1}\n  - day2: ${answer.day2}`));
 
 if (process.env.ENV === "test") {
-  if (answer !== 6) {
-    console.error(chalk.red.bold(`Test failed: expected 6 but got ${answer}`));
-  } else {
+  let error = false;
+  if (answer.day1 !== 3) {
+    console.error(chalk.red.bold(`Test failed for day 1: expected 3 but got ${answer.day1}`));
+    error = true;
+  }
+  if (answer.day2 !== 6) {
+    console.error(chalk.red.bold(`Test failed for day 2: should have passed 0 6 times but got ${answer.day2}`));
+    error = true;
+  }
+  if (!error) {
     console.log(chalk.green.bold("Test passed."));
   }
 }
